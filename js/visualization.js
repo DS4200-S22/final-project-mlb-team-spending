22457b94a0bbdcfcabc2fdfdce141277eb133e93
samdelfino <54412724+samdelfino@users.noreply.github.com>
2022-03-27 17:02:48 -0400

    x-axis

--- js/visualization.js
+++ js/visualization.js
@@ -4,14 +4,20 @@ const width = 900; //- margin.left - margin.right;
 const height = 650; //- margin.top - margin.bottom;
 
 // Append svg object to the body of the page to house Success Score Line Graph
-const svg1 = d3.select("#vis-holder")
+const svg1 = d3.select("#vis-container")
                 .append("svg")
                 .attr("width", width - margin.left - margin.right)
                 .attr("height", height - margin.top - margin.bottom)
                 .attr("viewBox", [0, 0, width, height]);
 
+
+// **IMPORTANT** Change out dummy hardcoded data 
 // Upload data
 d3.csv("data/MLB_Data_Cleaned.csv").then((data) => {
+  //Print first 10 rows of data 
+  for (var i = 0; i < 10; i++) {
+    console.log(data[i]);}
+
   let x1,y1;
 
   let xKey1, yKey1;
@@ -23,14 +29,44 @@ d3.csv("data/MLB_Data_Cleaned.csv").then((data) => {
 
     yKey1 = 'Success Score';
 
+    minX1 = d3.min(data, (d) => { return d[xKey1]; });
     maxX1 = d3.max(data, (d) => { return d[xKey1]; });
-    
-    console.log(maxX1);
-
+    console.log(minX1)
+    console.log(maxX1)
     // Create X scale
-    x1 = d3.scaleLinear()
-                .domain([0,maxX1])
-                .range([margin.left, width-margin.right]);
+    
+    x1 = d3.scaleTime()
+            .domain([new Date(minX1, 0, 1), new Date(maxX1, 0, 1)])
+            .range([margin.left, width - margin.right]);
+
+
+     // Add x axis
+     svg1.append("g")
+     .attr("transform", `translate(0,${height - margin.bottom})`)
+     .call(d3.axisBottom(x1))
+     .selectAll("text")  
+     .style("text-anchor", "end")
+     .attr("dx", "-.8em")
+     .attr("dy", ".15em")
+     .attr("transform", "rotate(-65)")
+     .attr("font-size", '20px')
+     .call((g) => g.append("text")
+                   .attr("x", width - margin.right)
+                   .attr("y", margin.bottom - 4)
+                   .attr("fill", "black")
+                   .attr("text-anchor", "end")
+                   .text(xKey1));
+     
+     /*append("g")
+     .attr("transform", `translate(0,${height - margin.bottom})`)
+     .call(d3.axisBottom(x1))
+     .attr("font-size", '20px')
+     .call((g) => g.append("text")
+                   .attr("x", width - margin.right)
+                   .attr("y", margin.bottom - 4)
+                   .attr("fill", "black")
+                   .attr("text-anchor", "end")
+                   .text(xKey1)*/
 
   }
    
